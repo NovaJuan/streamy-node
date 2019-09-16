94e5f87705ab57cd2707733d74ffe7e22b71ff72
imByzon <juanrm.works07@gmail.com>
2019-09-16 16:45:55 -0400

    please work 3.0

--- src/libs/multer.js
+++ src/libs/multer.js
@@ -4,7 +4,7 @@ import uuid from 'uuid/v4';
 
 const storage  = multer.diskStorage({
     destination:(req,file,cb) =>{
-        cb(null,'../../songs/');
+        cb(null,path.resolve('../../songs'));
     },
     filename:(req,file,cb)=>{
         cb(null,uuid() + path.extname(file.originalname));
